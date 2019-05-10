1313e1831b249a5630ee3c25916408580aa9e95a
Niccolò Olivieri <Zweer@users.noreply.github.com>
2019-05-10 12:52:51 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { config } from 'node-config-ts';
 
 import { AppModule } from './app/app.module';
 
+import { LoggerService } from './shared/utils/logger.service';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new LoggerService('Main'),
+  });
 
   await app.listen(config.server.port);
 }
